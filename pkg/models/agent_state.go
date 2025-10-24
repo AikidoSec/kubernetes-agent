e3fb70e50a2247ce749f8a7cf2e0dfba927ef6a4
NorbertHambuer <norbert@aikido.dev>
2025-10-24 12:37:18 +0300

    fix: Add lock for agent state read/write

--- pkg/models/agent_state.go
+++ pkg/models/agent_state.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type AgentState struct {
 	agentVersion               string
@@ -11,16 +14,22 @@ type AgentState struct {
 	excludedNamespaces         []string
 	monitoredResources         []string
 	controllerCacheSyncTimeout time.Duration
+
+	mu sync.Mutex
 }
 
 func NewEmptyAgentState() *AgentState {
 	return &AgentState{
 		excludedNamespaces: make([]string, 0),
 		monitoredResources: make([]string, 0),
+		mu:                 sync.Mutex{},
 	}
 }
 
 func (a *AgentState) SetInitialValues(agentVersion, agentNamespace, agentName, apiToken, apiEndpoint string, controllerCacheSyncTimeout time.Duration) *AgentState {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
 	a.agentVersion = agentVersion
 	a.agentNamespace = agentNamespace
 	a.agentName = agentName
@@ -31,65 +40,97 @@ func (a *AgentState) SetInitialValues(agentVersion, agentNamespace, agentName, a
 }
 
 func (a *AgentState) GetAgentVersion() string {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.agentVersion
 }
 
 func (a *AgentState) GetAgentNamespace() string {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.agentNamespace
 }
 
 func (a *AgentState) GetAgentName() string {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.agentName
 }
 
 func (a *AgentState) GetAPIToken() string {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.apiToken
 }
 
 func (a *AgentState) GetExcludedNamespaces() []string {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.excludedNamespaces
 }
 
 func (a *AgentState) GetMonitoredResources() []string {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.monitoredResources
 }
 
 func (a *AgentState) GetAPIEndpoint() string {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.apiEndpoint
 }
 
 func (a *AgentState) GetControllerCacheSyncTimeout() time.Duration {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	return a.controllerCacheSyncTimeout
 }
 
 func (a *AgentState) SetAgentVersion(version string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.agentVersion = version
 }
 
 func (a *AgentState) SetAgentNamespace(namespace string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.agentNamespace = namespace
 }
 
 func (a *AgentState) SetAgentName(name string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.agentName = name
 }
 
 func (a *AgentState) SetAPIToken(token string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.apiToken = token
 }
 
 func (a *AgentState) SetExcludedNamespaces(namespaces []string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.excludedNamespaces = namespaces
 }
 
 func (a *AgentState) SetMonitoredResources(resources []string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.monitoredResources = resources
 }
 
 func (a *AgentState) SetAPIEndpoint(endpoint string) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.apiEndpoint = endpoint
 }
 
 func (a *AgentState) SetControllerCacheSyncTimeout(timeout time.Duration) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.controllerCacheSyncTimeout = timeout
 }
