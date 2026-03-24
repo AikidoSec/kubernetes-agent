480f56b82991601f7e63229c099d0982a163adc0
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-03-24 16:27:48 +0000

    refactor: create cluster_identifier.go

--- internal/services/manager/manager.go
+++ internal/services/manager/manager.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -24,8 +23,6 @@ import (
 	"aikidoSec.kubernetesAgent/pkg/batchclient"
 	"aikidoSec.kubernetesAgent/pkg/imagescache"
 	"aikidoSec.kubernetesAgent/pkg/models"
-	"github.com/google/uuid"
-	"go.uber.org/multierr"
 	"gopkg.in/yaml.v3"
 	corev1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
@@ -40,8 +37,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
-
-var noHostErrorMessage = "no such host"
 
 const (
 	defaultAgentVersion = "1.0.0"
@@ -769,173 +764,6 @@ func (s *Service) configureSBOMCollectorDeployment(ctx context.Context, enabled,
 		return fmt.Errorf("error updating SBOM collector deployment: %w", err)
 	}
 	return nil
-}
-
-// GetClusterIdentifier extracts the unique identifier for the Kubernetes cluster
-func (s *Service) GetClusterIdentifier(ctx context.Context) (string, error) {
-	var errs error
-	// Check if the cluster is GKE
-	identifier, err := s.GetGKEClusterIdentifier(ctx)
-	if err != nil {
-		errs = multierr.Append(errs, err)
-	}
-
-	if identifier != "" {
-		return identifier, errs
-	}
-
-	// Check if the cluster is AKS
-	identifier, err = s.GetAKSClusterIdentifier(ctx)
-	if err != nil {
-		errs = multierr.Append(errs, err)
-	}
-
-	if identifier != "" {
-		return identifier, errs
-	}
-
-	// Try to get the identifier from the kube-proxy configmap
-	identifier, err = s.GetClusterIdentifierFromProxy(ctx)
-	if err != nil {
-		errs = multierr.Append(errs, err)
-	}
-
-	if identifier != "" {
-		return identifier, errs
-	}
-
-	// Try to get the `kube-system` namespace UID
-	identifier, err = s.GetKubeSystemNamespaceUID(ctx)
-	if err != nil {
-		errs = multierr.Append(errs, err)
-	}
-
-	if identifier != "" {
-		return identifier, errs
-	}
-
-	// If all methods fail, return a random UUID to ensure the cluster can still be uniquely identified.
-	return uuid.New().String(), multierr.Append(errs, fmt.Errorf("could not get unique cluster identifier"))
-}
-
-// GetGKEClusterIdentifier checks if the Kubernetes cluster is GKE and returns the cluster uid if true.
-func (s *Service) GetGKEClusterIdentifier(ctx context.Context) (string, error) {
-	req, err := http.NewRequestWithContext(ctx, "GET",
-		"http://metadata.google.internal/computeMetadata/v1/instance/attributes/cluster-uid", nil)
-	if err != nil {
-		return "", fmt.Errorf("error creating GKE metadata request: %w", err)
-	}
-
-	req.Header.Add("Metadata-Flavor", "Google")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		if strings.Contains(err.Error(), noHostErrorMessage) {
-			// Not a GKE cluster
-			return "", nil
-		}
-
-		return "", fmt.Errorf("error getting cluster uid: %w", err)
-	}
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			s.logger.ReportError(ctx, err, "error closing GKE metadata response body", "managerError")
-		}
-	}()
-
-	if resp.StatusCode == http.StatusNotFound {
-		// Not a GKE cluster
-		return "", nil
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("unexpected status code from GKE metadata server: %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("error reading GKE metadata response body: %w", err)
-	}
-
-	clusterUID := string(body)
-	return clusterUID, nil
-}
-
-// GetAKSClusterIdentifier checks if the Kubernetes cluster is AKS and returns the DNS name if true.
-func (s *Service) GetAKSClusterIdentifier(ctx context.Context) (string, error) {
-	// Get the kube-proxy pods in the kube-system namespace
-	pods, err := s.kubernetesClientSet.CoreV1().Pods("kube-system").List(ctx, v1.ListOptions{
-		LabelSelector: "component=kube-proxy,kubernetes.azure.com/managedby=aks",
-	})
-	if err != nil {
-		return "", fmt.Errorf("error getting kube-proxy pods: %w", err)
-	}
-
-	// Iterate through all kube-proxy pods
-	for _, pod := range pods.Items {
-		// Check each environment variable in each container
-		for _, container := range pod.Spec.Containers {
-			for _, env := range container.Env {
-				if env.Name != "KUBERNETES_SERVICE_HOST" {
-					continue
-				}
-
-				// Check if the AKS DNS name is present
-				if len(env.Value) == 0 {
-					continue
-				}
-
-				return env.Value, nil
-			}
-		}
-	}
-
-	return "", nil
-}
-
-// GetClusterIdentifierFromProxy extracts the unique identifier for the Kubernetes cluster from the kube-proxy ConfigMap
-func (s *Service) GetClusterIdentifierFromProxy(ctx context.Context) (string, error) {
-	configMap, err := s.kubernetesClientSet.CoreV1().ConfigMaps("kube-system").Get(ctx, "kube-proxy", v1.GetOptions{})
-	if err != nil {
-		// kube-proxy is not installed in this cluster
-		if k8sErrors.IsNotFound(err) {
-			return "", nil
-		}
-		return "", fmt.Errorf("error getting kube-proxy configmap: %w", err)
-	}
-
-	// Extract the kubeconfig content if exists
-	for _, v := range configMap.Data {
-		// Try to load the kubeconfig content
-		config, err := clientcmd.Load([]byte(v))
-		if err != nil {
-			continue
-		}
-
-		// Get the current context
-		contextName := config.CurrentContext
-		ctx, ok := config.Contexts[contextName]
-		if !ok {
-			continue
-		}
-
-		// Get the cluster information
-		cluster, ok := config.Clusters[ctx.Cluster]
-		if ok {
-			return cluster.Server, nil
-		}
-	}
-
-	return "", nil
-}
-
-func (s *Service) GetKubeSystemNamespaceUID(ctx context.Context) (string, error) {
-	// Get the `kube-system` namespace
-	ns, err := s.kubernetesClientSet.CoreV1().Namespaces().Get(ctx, "kube-system", v1.GetOptions{})
-	if err != nil {
-		return "", fmt.Errorf("error getting `kube-system` namespace: %w", err)
-	}
-
-	return string(ns.UID), nil
 }
 
 func (s *Service) GetDeploymentAndChartsVersions(ctx context.Context, ns, deploymentName string) (string, string, error) {
