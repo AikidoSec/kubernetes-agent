5c844bbd359e8b4e04e6f67f4f746db4b7cd7d9e
NorbertHambuer <norbert@aikido.dev>
2026-09-23 15:59:48 +0300

    fix: Add nolint:staticcheck for Endpoints

--- internal/predicates/endpoints.go
+++ internal/predicates/endpoints.go
@@ -46,12 +46,15 @@ func NewEndpointsPredicates(nsFilter *NamespaceFilter) predicate.Predicate {
 func endpointsFromUnstructured(obj client.Object) (v1.Endpoints, error) {
 	unstructuredObj, ok := obj.(*unstructured.Unstructured)
 	if !ok {
+		//nolint:staticcheck
 		return v1.Endpoints{}, nil
 	}
 
+	//nolint:staticcheck
 	var endpoints v1.Endpoints
 	err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredObj.UnstructuredContent(), &endpoints)
 	if err != nil {
+		//nolint:staticcheck
 		return v1.Endpoints{}, err
 	}
 
